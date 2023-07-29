d60862cf729b81040a51c55a6977edb3b41298e0
mollytodd <131923397+mollytodd@users.noreply.github.com>
2023-07-29 11:50:14 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,28 +1,54 @@
-const cats = ["Milo", "Otis", "Garfield"];
-function destructivelyAppendCat(name) {
-    cats.push(name);
+const cats = ["Milo", "Otis", "Garfield"]
 
-}
-function destructivelyPrependCat(name) {
-    cats.unshift(name);
-}
-function destructivelyRemoveLastCat(name) {
-    cats.pop(name);
-}
-function destructivelyRemoveFirstCat(name) {
-    cats.shift(name);
-}
-function appendCat(name) {
-    return [...cats, name];
+console.log(cats)
 
+function destructivelyAppendCat() {
+    cats.push('Ralph')
+    return cats
 }
-function prependCat(name) {
-    return [name, ...cats];
+console.log(destructivelyAppendCat())
+
+function destructivelyPrependCat(catName) {
+    cats.unshift('Bob')
+    return cats
 }
+
+console.log(destructivelyPrependCat())
+
+function destructivelyRemoveLastCat() {
+    cats.pop()
+    return cats
+}
+
+console.log(destructivelyRemoveLastCat())
+
+function destructivelyRemoveFirstCat() {
+    cats.shift()
+    return cats
+}
+
+console.log(destructivelyRemoveFirstCat())
+
+function appendCat() {
+    return [...cats, "Broom"]
+   
+}
+console.log(appendCat())
+
+function prependCat() {
+    return ["Arnold", ...cats]
+}
+
+console.log(prependCat())
+
 function removeLastCat() {
-return cats.slice(0,-1);
+   return [...cats.slice(0,2)]
 }
+
+console.log (removeLastCat())
 
 function removeFirstCat() {
-    return cats.slice(1);
+    return [...cats.slice(1)]
 }
+
+console.log (removeFirstCat())
